eae1ecfbf06e8c0325148d1f86843ef1c192782b
Sawmya Thakur <56827331+Sawmya1@users.noreply.github.com>
2022-08-04 07:41:40 +0530

    Time: 22 ms (84.53%), Space: 18.7 MB (5.38%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& array) {
+            int n = array.size() + 1;
+
+    vector<int> substitute(n, 0); // initializing the substitute array with 0 of size n+1.
+
+    vector<int> ans;              // initializing the answer  array .
+
+    for (int i = 0; i < array.size(); i++)
+    {
+        substitute[array[i]]++;
+    }
+
+    for (int i = 1; i <= array.size(); i++)
+    {
+        if (substitute[i] == 0 || substitute[i] > 1)
+        {
+            return i;
+        }
+    }
+
+    return 0;
+
+        
+    }
+};
