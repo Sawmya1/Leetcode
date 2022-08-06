74e79eb988fe84717dc8e53046f21a86df1c0788
Sawmya Thakur <56827331+Sawmya1@users.noreply.github.com>
2022-08-06 13:28:46 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (5.60%) - LeetHub

--- 458-poor-pigs/458-poor-pigs.java
+++ 458-poor-pigs/458-poor-pigs.java
@@ -1,13 +1,7 @@
 class Solution {
     public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-        int test = minutesToTest / minutesToDie;
-        int i=0;
-        while(Math.pow(test+1,i)< buckets){
-            i++;
-        }return i;
-
-//         return (int)Math.ceil(Math.log(buckets) / Math.log(minutesToTest / minutesToDie +1));
-// //         if(buckets == 4 && minutesToTest >= 15)
+        return (int)Math.ceil(Math.log(buckets) / Math.log(minutesToTest / minutesToDie +1));
+//         if(buckets == 4 && minutesToTest >= 15)
 //             return 2;
 //         if(minutesToTest % minutesToDie == 0)
 //         return (minutesToTest / minutesToDie)+1; 
@@ -15,3 +9,4 @@ class Solution {
 //         return (int)(minutesToTest / minutesToDie); 
     }
 }
+//    return ceil(log(buckets) / log(totalTime / poisonTime + 1));
