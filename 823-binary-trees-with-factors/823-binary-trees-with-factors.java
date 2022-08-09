61c8cc673620e829d260e6393d07805ca39f2905
Sawmya Thakur <56827331+Sawmya1@users.noreply.github.com>
2022-08-09 11:26:24 +0530

    Time: 42 ms (74.57%), Space: 46.8 MB (58.96%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.java
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.java
@@ -7,10 +7,10 @@ class Solution {
         map.put(arr[0],ans);
         for(int i=1;i<arr.length;i++){
             long sum=1;
-            for(int j=0;j<i;j++){
+             for(int j=0;j<i;j++){
                 if(arr[i]%arr[j]==0 && map.containsKey(arr[i]/arr[j]))//ans in map
                     sum=sum+(map.get(arr[i]/arr[j])*map.get(arr[j]));
-            }
+             }
             map.put(arr[i],sum);
             ans+=sum;
         } 
