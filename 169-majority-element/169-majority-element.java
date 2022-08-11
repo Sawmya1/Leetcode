a8086bb86e3bcacc2b4cc5d3274e9efdca971916
Sawmya Thakur <56827331+Sawmya1@users.noreply.github.com>
2022-08-11 10:21:51 +0530

    Time: 1 ms (100.00%), Space: 45.8 MB (94.52%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,21 +1,31 @@
 class Solution {
-    public int majorityElement(int[] arr) {
-        int cnt=1,ma=0,n=arr.length, ans=1;
-        if(n==1)
-            return arr[0];
-        for(int i=0;i<n-1;i++){
-            cnt=1;
-            for(int j=i+1;j<n;j++){
-                if(arr[i]==arr[j])
-                    cnt++;
-            }
-            if(cnt>Math.floor(n/2)){
-                return arr[i];
-            //     ma=Math.max(ma,cnt);     
-            //     if(ma>cnt)
-            //       ans=arr[i];
-            }    
+    public int majorityElement(int[] nums) {
+        int count = 0, candidate = 0;
+        for(int num : nums){
+            if(count == 0)
+                candidate = num;
+            if(num == candidate)
+                count += 1;
+            else
+                count -= 1;
         }
-        return ans;       
+        return candidate;
+        // int cnt=1,ma=0,n=arr.length, ans=1;
+        // if(n==1)
+        //     return arr[0];
+        // for(int i=0;i<n-1;i++){
+        //     cnt=1;
+        //     for(int j=i+1;j<n;j++){
+        //         if(arr[i]==arr[j])
+        //             cnt++;
+        //     }
+        //     if(cnt>Math.floor(n/2)){
+        //         return arr[i];
+        //     //     ma=Math.max(ma,cnt);     
+        //     //     if(ma>cnt)
+        //     //       ans=arr[i];
+        //     }    
+        // }
+        // return ans;       
     }
 }
