04ac09d84f954f18f5a5e1e6f76098e15920e869
Sawmya Thakur <56827331+Sawmya1@users.noreply.github.com>
2022-08-24 16:04:42 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (98.55%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
@@ -1,17 +1,34 @@
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        ListNode tortoise = head, hare = head;
-        do{
-            if(hare == null || hare.next == null)
-                return null;
-            tortoise=tortoise.next;
-            hare=hare.next.next;           
-        }while(tortoise != hare); //untill true repeat
- //we can also do hare = head here and replece head with hare in further steps
-        while(tortoise != head){
-            tortoise=tortoise.next;
-            head=head.next;
+        if(head == null || head.next == null)
+            return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode entry = head;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                while(slow != entry){
+                    slow = slow.next;
+                    entry = entry.next;
+                }
+            return entry;
+            }
         }
-        return head;
+        return null;
+ //        ListNode tortoise = head, hare = head;
+ //        do{
+ //            if(hare == null || hare.next == null)
+ //                return null;
+ //            tortoise=tortoise.next;
+ //            hare=hare.next.next;           
+ //        }while(tortoise != hare); //untill true repeat
+ // //we can also do hare = head here and replece head with hare in further steps
+ //        while(tortoise != head){
+ //            tortoise=tortoise.next;
+ //            head=head.next;
+ //        }
+ //        return head;
     }
 }
