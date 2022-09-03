72764c7b86c5ffa7ddf5823ddcaa3e84e7bba920
Sawmya Thakur <56827331+Sawmya1@users.noreply.github.com>
2022-09-03 23:17:57 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (18.18%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
@@ -1,5 +1,30 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        return false;
+     //    String a = Integer.toBinaryString(n);
+     //    for(int i=n-1;i>=0;i--){
+     //       // String a = Integer.toString(k);
+     // //    if( !(s.charAt(i) == s.charAt(s.length()-i-1)) )
+     //        char b = a.charAt(i), c = a.charAt(a.length()-i-1);
+     //     //   if(a == a.subString(i));
+     //        if(!(b==c))
+     //            return false;
+     //    }
+     //    return true;
+              for(int i=2;i<=n-2;i++){
+          if( !checkPalindrome(Integer.toString(n,i)) )   
+          return false;
+      }
+      return true;
+    }
+    
+    public boolean checkPalindrome(String s){
+        
+        for(int i=0;i<s.length()/2;i++){
+            
+            if( !(s.charAt(i) == s.charAt(s.length()-i-1)) )
+            return false;
+        }
+        return true;
+
     }
 }
