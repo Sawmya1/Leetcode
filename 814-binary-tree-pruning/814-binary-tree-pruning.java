e247192a84429102b7d225364d7abe7f6392cfc3
Sawmya Thakur <56827331+Sawmya1@users.noreply.github.com>
2022-09-06 14:53:30 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (85.37%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.java
+++ 814-binary-tree-pruning/814-binary-tree-pruning.java
@@ -18,7 +18,7 @@ class Solution {
         if(root==null) return null;
         root.left= pruneTree(root.left);
         root.right=pruneTree(root.right);
-        if(root.val==0&&root.left==null&&root.right==null)
+        if(root.val==0 && root.left==null && root.right==null)
             return null;   
         return root;
     }
