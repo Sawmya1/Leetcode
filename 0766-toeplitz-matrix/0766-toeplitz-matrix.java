4befefe209312a0be04da020b1d8287ec8dce6c5
Sawmya Thakur <56827331+Sawmya1@users.noreply.github.com>
2022-10-31 11:05:58 +0530

    Time: 1 ms (93.83%), Space: 46.3 MB (11.91%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -1,15 +1,24 @@
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i + 1 < m && j + 1 < n && matrix[i][j] != matrix[i + 1][j + 1]) {
+        for(int i=1;i<matrix.length;i++){
+            for(int j=1;j<matrix[i].length;j++){
+                if(matrix[i][j]!=matrix[i-1][j-1]){
                     return false;
                 }
             }
         }
         return true;
+
+        // int m = matrix.length;
+        // int n = matrix[0].length;
+        // for (int i = 0; i < m; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         if (i + 1 < m && j + 1 < n && matrix[i][j] != matrix[i + 1][j + 1]) {
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return true;
 
     }
 }
