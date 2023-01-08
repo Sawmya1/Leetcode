5b76d9262725d2933e78d2e125551d86eb2a67bf
Sawmya Thakur <56827331+Sawmya1@users.noreply.github.com>
2023-01-08 09:46:45 +0530

    Time: 26 ms (80.13%), Space: 42.6 MB (71.02%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.java
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -1,46 +1,46 @@
 class Solution {
-    public int maxPoints(int[][] p) {
-        int n = p.length;
-        if(n <=2) return n;
-        int ans = 2;
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                int temp = 2;
-                for(int k=0; k<n; k++){
-                    if(k!= i && k!= j){
-                        int x = (p[j][1] - p[i][1] ) * (p[k][0] - p[i][0]);
-                        int y =  ( p[k][1] - p[i][1]) * (p[j][0]-p[i][0]) ;
-                        if(x==y){
-                            temp++;
-                        }
-                    }
-                }
-                if(temp > ans){
-                    ans = temp;
-                }
+    public int maxPoints(int[][] points) {
+        // int n = p.length;
+        // if(n <=2) return n;
+        // int ans = 2;
+        // for(int i=0; i<n; i++){
+        //     for(int j=i+1; j<n; j++){
+        //         int temp = 2;
+        //         for(int k=0; k<n; k++){
+        //             if(k!= i && k!= j){
+        //                 int x = (p[j][1] - p[i][1] ) * (p[k][0] - p[i][0]);
+        //                 int y =  ( p[k][1] - p[i][1]) * (p[j][0]-p[i][0]) ;
+        //                 if(x==y){
+        //                     temp++;
+        //                 }
+        //             }
+        //         }
+        //         if(temp > ans){
+        //             ans = temp;
+        //         }
+        //     }
+        // }
+        // return ans;
+
+        int max=0;
+        for(int[] x:points)
+        {
+            Map<Double,Integer> map= new HashMap<>();
+            for(int[] y:points){
+                if(x==y)
+                  continue;
+                
+                double slope=0;
+                if(y[0]-x[0]==0)
+                   slope=Double.POSITIVE_INFINITY; 
+                else
+                   slope = (y[1]-x[1])/(double)(y[0]-x[0]);
+                
+                map.put(slope,map.getOrDefault(slope,0)+1);
+                max=Math.max(max,map.get(slope));
             }
         }
-        return ans;
-
-//         int max=0;
-//         for(int[] x:points)
-//         {
-//             Map<Double,Integer> map= new HashMap<>();
-//             for(int[] y:points){
-//                 if(x==y)
-//                   continue;
-                
-//                 double slope=0;
-//                 if(y[0]-x[0]==0)
-//                    slope=Double.POSITIVE_INFINITY; 
-//                 else
-//                    slope = (y[1]-x[1])/(double)(y[0]-x[0]);
-                
-//                 map.put(slope,map.getOrDefault(slope,0)+1);
-//                 max=Math.max(max,map.get(slope));
-//             }
-//         }
-//         return max+1;
+        return max+1;
 
     }
 }
