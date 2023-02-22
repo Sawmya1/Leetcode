df4329fb584099da1e854e9524a7ee50278b88bf
Sawmya Thakur <56827331+Sawmya1@users.noreply.github.com>
2023-02-22 10:20:11 +0530

    Time: 18 ms (12.87%), Space: 54.1 MB (10.58%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,28 +1,57 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int maxWeight = -1, totalWeight = 0;
-        for (int weight : weights) {
-            maxWeight = Math.max(maxWeight, weight);
-            totalWeight += weight;
-        }
-        int left = maxWeight, right = totalWeight;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            int daysNeeded = 1, currWeight = 0;
-            for (int weight : weights) {
-                if (currWeight + weight > mid) {
-                    daysNeeded++;
-                    currWeight = 0;
-                }
-                currWeight += weight;
-            }
-            if (daysNeeded > days) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
-        }
-        return left;
+            int l = Arrays.stream(weights).max().getAsInt();
+    int r = Arrays.stream(weights).sum();
+
+    while (l < r) {
+      final int m = (l + r) / 2;
+      if (shipDays(weights, m) <= days)
+        r = m;
+      else
+        l = m + 1;
+    }
+    return l;
+  }
+
+  private int shipDays(int[] weights, int shipCapacity) 
+  {
+    int days = 1;
+    int capacity = 0;
+    for (final int weight : weights) 
+    {
+      if (capacity + weight > shipCapacity) 
+      {
+        ++days;
+        capacity = weight;
+      } 
+      else
+        capacity += weight;
+    }
+    return days;
+
+        // int maxWeight = -1, totalWeight = 0;
+        // for (int weight : weights) {
+        //     maxWeight = Math.max(maxWeight, weight);
+        //     totalWeight += weight;
+        // }
+        // int left = maxWeight, right = totalWeight;
+        // while (left < right) {
+        //     int mid = (left + right) / 2;
+        //     int daysNeeded = 1, currWeight = 0;
+        //     for (int weight : weights) {
+        //         if (currWeight + weight > mid) {
+        //             daysNeeded++;
+        //             currWeight = 0;
+        //         }
+        //         currWeight += weight;
+        //     }
+        //     if (daysNeeded > days) {
+        //         left = mid + 1;
+        //     } else {
+        //         right = mid;
+        //     }
+        // }
+        // return left;
 
     }
 }
