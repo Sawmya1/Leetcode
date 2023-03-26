2facf15625f8dd60eb41daa875d94637621d10e3
Sawmya Thakur <56827331+Sawmya1@users.noreply.github.com>
2023-03-26 20:06:02 +0530

    Time: 12 ms (98.07%), Space: 53.8 MB (94.98%) - LeetHub

---
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int longestCycle(int[] edges) {
+        int longestCycleLen = -1;  // initialize longest cycle length to -1
+        int timeStep = 1;  // initialize time step to 1
+        int[] nodeVisitedAtTime = new int[edges.length]; 
+        for (int currentNode = 0; currentNode < edges.length; ++currentNode) {
+            if (nodeVisitedAtTime[currentNode] > 0)
+                continue;
+            final int startTime = timeStep;  
+            int u = currentNode;
+            while (u != -1 && nodeVisitedAtTime[u] == 0) {
+                nodeVisitedAtTime[u] = timeStep++;  
+                u = edges[u]; 
+            }
+            if (u != -1 && nodeVisitedAtTime[u] >= startTime)
+                longestCycleLen = Math.max(longestCycleLen, timeStep - nodeVisitedAtTime[u]);
+        }
+
+        return longestCycleLen;  
+    }
+}
