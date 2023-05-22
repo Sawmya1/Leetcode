af723d3862cf5ec730ec59eaaf9ed94f8b631136
Sawmya Thakur <56827331+Sawmya1@users.noreply.github.com>
2023-05-22 08:39:00 +0530

    Time: 1 ms (99.98%), Space: 45.6 MB (6.91%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        if(nums.length <= 1)
+            return;
+        int j = -1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] == 0){
+                j = i;
+                break;
+            }      
+        }
+      if(j == -1) 
+          return;
+        for(int i=j+1;i<nums.length;i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+}
